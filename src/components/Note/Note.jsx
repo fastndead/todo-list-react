53fc407a7b5ece607790e767a1e86bced2eaab62
Asman Umbetov <aumbetov@griddynamics.com>
2020-04-18 20:41:41 +0400

    new buttons to notes and react-spring

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,30 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { useSpring, animated } from 'react-spring';
 import { markNote, deleteNote } from '../../redux/actions/actions';
 import './Note.scss';
 
-const Note = ({ note, markNote, deleteNote }) => {
-  const handleMark = () => markNote(note.id);
-  const handleDelete = () => deleteNote(note.id);
+const Note = ({ note, markNoteProps, deleteNoteProps }) => {
+  // Animation Land
+  const handleMark = () => markNoteProps(note.id);
+  const handleDelete = () => deleteNoteProps(note.id);
+
+  const markButtonProps = useSpring({ backgroundColor: note.marked ? '#34495E' : '#fff', color: note.marked ? '#fff' : '#34495E' });
 
   return (
     <div className="note-wrapper">
-      <div className="note-container">
-        <button
-          type="button"
-          className={`note-mark ${note.marked ? 'note-mark_marked' : ''}`}
-          onClick={handleMark}
-        />
-        <div contentEditable className="note-text">
+      <div
+        className="note-container"
+      >
+        <div className="note-text">
           {note.text}
         </div>
-        <button
-          type="button"
-          className="note-delete"
-          onClick={handleDelete}
-        >
-          Delete
-        </button>
+        <div className="button-container">
+          <animated.button
+            type="button"
+            className="note-mark"
+            onClick={handleMark}
+            style={markButtonProps}
+          >
+            Mark
+          </animated.button>
+          <button
+            type="button"
+            className="note-delete"
+            onClick={handleDelete}
+          >
+            Delete
+          </button>
+        </div>
       </div>
     </div>
 
@@ -32,8 +43,8 @@ const Note = ({ note, markNote, deleteNote }) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  markNote: (id) => dispatch(markNote(id)),
-  deleteNote: (id) => dispatch(deleteNote(id)),
+  markNoteProps: (id) => dispatch(markNote(id)),
+  deleteNoteProps: (id) => dispatch(deleteNote(id)),
 });
 
 export default connect(null, mapDispatchToProps)(Note);
