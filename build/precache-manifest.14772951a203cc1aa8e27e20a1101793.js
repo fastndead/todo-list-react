a16150cd026287ef42affeb0075113026db437ce
Asman Umbetov <asman.umbetov@sbermarket.ru>
2023-04-07 09:40:35 +0300

    added bundle to the project

---
+++ build/precache-manifest.14772951a203cc1aa8e27e20a1101793.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64445434930112194c51e8c910712519",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0e1954772b472f8af8a",
+    "url": "/static/css/main.d8e8e054.chunk.css"
+  },
+  {
+    "revision": "b459b2eed9914b4c7f56",
+    "url": "/static/js/2.7d73b121.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7d73b121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e1954772b472f8af8a",
+    "url": "/static/js/main.e7103887.chunk.js"
+  },
+  {
+    "revision": "2fe744c50649b9cf0f3c",
+    "url": "/static/js/runtime-main.59a10de9.js"
+  },
+  {
+    "revision": "d5000b2b6eb196e7b38f901358ab2a27",
+    "url": "/static/media/submit.d5000b2b.svg"
+  }
+]);
