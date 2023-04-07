09cf39fba21ebdbc653addbb22de4ae6629f9bb9
Asman Umbetov <asman.umbetov@sbermarket.ru>
2023-04-07 10:01:41 +0300

    Updates

---
+++ precache-manifest.efedeed1dfcfc8ccc74ca39f4bfb45a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a35757deb7ac8a7765ea018cb8bab64",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "e52eb8c675dda5523bda",
+    "url": "/todo-list-react/static/css/main.d8e8e054.chunk.css"
+  },
+  {
+    "revision": "b459b2eed9914b4c7f56",
+    "url": "/todo-list-react/static/js/2.7d73b121.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-list-react/static/js/2.7d73b121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e52eb8c675dda5523bda",
+    "url": "/todo-list-react/static/js/main.2349323d.chunk.js"
+  },
+  {
+    "revision": "a79fddd6b6f71840ac57",
+    "url": "/todo-list-react/static/js/runtime-main.d8c46d2a.js"
+  },
+  {
+    "revision": "d5000b2b6eb196e7b38f901358ab2a27",
+    "url": "/todo-list-react/static/media/submit.d5000b2b.svg"
+  }
+]);
